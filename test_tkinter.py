072317722ac2b7ed342e528da6aacd2e49793978
Ocean634 <clenetjules@gmail.com>
2023-09-17 13:57:07 +0200

    Cleaning tkinter in a class

--- test_tkinter.py
+++ test_tkinter.py
@@ -14,35 +14,39 @@ except ImportError:
     raise ImportError ["tkinter module not installed"]
 
 
-def create_window():
-    """ Create tkinter root window
+class Displayer:
 
-    Returns:
-        Tk_instance: main window of the application
-    """
-    Master = tkinter.Tk()
-    Master.state("zoomed")
-    Master.update()
-    return Master
+    def __init__(self):
+        self.Master = self.create_window()
+        self.Canvas = self.create_canvas(self.Master)
+        self.Master.mainloop()
 
 
-def create_canvas(Master):
-    """ Create a drawing space in the window
+    def create_window(self):
+        """ Create tkinter root window
 
-    Returns:
-        Canvas_instance: Drawing surface
-    """
-    Frm = tkinter.Frame(Master)
-    Frm.pack()
-    Canvas = tkinter.Canvas(Frm,
-                            bg="black",
-                            height=Master.winfo_height()-4,
-                            width=Master.winfo_width()-4
-                           )
-    Canvas.pack()
-    Master.update()
-    return Canvas
+        Returns:
+            Tk_instance: main window of the application
+        """
+        self.Master = tkinter.Tk()
+        self.Master.state("zoomed")
+        self.Master.update()
+        return self.Master
 
-Master = create_window()
-Canvas = create_canvas(Master)
-Master.mainloop()
+
+    def create_canvas(self, Master):
+        """ Create a drawing space in the window
+
+        Returns:
+            Canvas_instance: Drawing surface
+        """
+        self.Frm = tkinter.Frame(self.Master)
+        self.Frm.pack()
+        self.Canvas = tkinter.Canvas(self.Frm,
+                                     bg="black",
+                                     height=Master.winfo_height()-4,
+                                     width=Master.winfo_width()-4
+                                    )
+        self.Canvas.pack()
+        self.Master.update()
+        return self.Canvas
